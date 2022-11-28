0abc34ddc7c0a27b6c4bbf16bbb41035fd166c4b
Ithihas Madala <helloithihas@gmail.com>
2022-11-28 18:11:07 +0530

    Axios

--- coin_new.js
+++ coin_new.js
@@ -1,15 +1,90 @@
-const token = 'coinranking86b761a84874747cae3bf5199405626ea0bd1959c3b98a94';
-const request = new XMLHttpRequest();
-request.open('GET', 'https://api.coinranking.com/v2/search-suggestions?query=bitcoin');
-request.setRequestHeader("Access-Control-Allow-Origin", "*");
-request.setRequestHeader("Content-Type", "application/json");
-request.setRequestHeader('x-access-token', 'coinranking86b761a84874747cae3bf5199405626ea0bd1959c3b98a94');
-request.send();
-request.onload = function() {
-  console.log(request);
-  if (request.status === 200 ) {
-    console.log(request.response)
-  } else {
-    console.log(request.status)
+//const axios = require('axios');
+let response = null;
+
+// Builds the HTML Table out of myList.
+
+
+// Adds a header row to the table and returns the set of columns.
+// Need to do union of keys from all records as some records may not contain
+// all records.
+
+new Promise(async (resolve, reject) => {
+  try {
+    response = await axios.get('https://api.coinranking.com/v2/search-suggestions?query=bit', {
+      headers: {
+        'x-access-token': 'coinranking86b761a84874747cae3bf5199405626ea0bd1959c3b98a94',
+      },
+    });
+  } catch(ex) {
+    response = null;
+    // error
+    console.log(ex);
+    reject(ex);
   }
+  if (response) {
+    // success
+    const json = response.data;
+    table_data = json.data.coins
+
+    document.body.appendChild(buildHtmlTable([{
+      "name": "abc",
+      "age": 50
+    },
+    {
+      "age": "25",
+      "hobby": "swimming"
+    },
+    {
+      "name": "xyz",
+      "hobby": "programming"
+    }
+  ]));
+    console.log(table_data);
+    resolve(json);
+  }
+});
+
+
+var _table_ = document.createElement('table'),
+  _tr_ = document.createElement('tr'),
+  _th_ = document.createElement('th'),
+  _td_ = document.createElement('td');
+
+// Builds the HTML Table out of myList json data from Ivy restful service.
+function buildHtmlTable(arr) {
+  var table = _table_.cloneNode(false),
+    columns = addAllColumnHeaders(arr, table);
+  for (var i = 0, maxi = arr.length; i < maxi; ++i) {
+    var tr = _tr_.cloneNode(false);
+    for (var j = 0, maxj = columns.length; j < maxj; ++j) {
+      var td = _td_.cloneNode(false);
+      var cellValue = arr[i][columns[j]];
+      td.appendChild(document.createTextNode(arr[i][columns[j]] || ''));
+      tr.appendChild(td);
+    }
+    table.appendChild(tr);
+  }
+  return table;
 }
+
+// Adds a header row to the table and returns the set of columns.
+// Need to do union of keys from all records as some records may not contain
+// all records
+function addAllColumnHeaders(arr, table) {
+  var columnSet = [],
+    tr = _tr_.cloneNode(false);
+  for (var i = 0, l = arr.length; i < l; i++) {
+    for (var key in arr[i]) {
+      if (arr[i].hasOwnProperty(key) && columnSet.indexOf(key) === -1) {
+        columnSet.push(key);
+        var th = _th_.cloneNode(false);
+        th.appendChild(document.createTextNode(key));
+        tr.appendChild(th);
+      }
+    }
+  }
+  table.appendChild(tr);
+  return columnSet;
+}
+
+
