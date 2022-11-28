7525e3097cd4d0a2873879d11793d1a2d43d9582
Ithihas Madala <helloithihas@gmail.com>
2022-11-28 17:14:57 +0530

    Switched back to XMLHttpRequest

--- coin_new.js
+++ coin_new.js
@@ -1,13 +1,15 @@
-fetch('api/Sessions', {
-    method: 'POST',
-    headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'x-access-token': 'coinranking86b761a84874747cae3bf5199405626ea0bd1959c3b98a94'
-    },
-    body: JSON.stringify('https://api.coinranking.com/v2/search-suggestions?query=bitcoin')
-})
-.then(response => response.text())
-.then((response) => updateResponse(response))
-.catch(error => console.error(error));
+const token = 'coinranking86b761a84874747cae3bf5199405626ea0bd1959c3b98a94';
+const request = new XMLHttpRequest();
+request.open('GET', 'https://api.coinranking.com/v2/search-suggestions?query=bitcoin');
+request.setRequestHeader("Access-Control-Allow-Origin", "*");
+request.setRequestHeader("Content-Type", "application/json");
+request.setRequestHeader('x-access-token', 'coinranking86b761a84874747cae3bf5199405626ea0bd1959c3b98a94');
+request.send();
+request.onload = function() {
+  console.log(request);
+  if (request.status === 200 ) {
+    console.log(request.response)
+  } else {
+    console.log(request.status)
+  }
+}
